79076115dedd6ebc8650aae1bfad169367356e2b
karl-denby <karl.denby@gmail.com>
2017-01-20 14:39:41 +0000

    2 basic activities/screens no polish

--- app/src/main/java/com/example/android/myapplication/MovieGrid.java
+++ app/src/main/java/com/example/android/myapplication/MovieGrid.java
@@ -1,7 +1,11 @@
 package com.example.android.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MovieGrid extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MovieGrid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_grid);
+
+        Button test_btn = (Button) findViewById(R.id.test_button);
+        test_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDetailsForMovie(1);
+            }
+        });
+    }
+
+    void showDetailsForMovie(int movieId) {
+        Context context = this;
+
+        Intent intent = new Intent(context, MovieDetail.class);
+        intent.putExtra("ID", movieId);
+        startActivity(intent);
     }
 }
