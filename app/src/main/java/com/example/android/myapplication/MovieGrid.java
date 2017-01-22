6bee994849257f963fd30d9c2babb3beabeb0ed2
karl-denby <karl.denby@gmail.com>
2017-01-22 15:30:26 +0000

    Grid of images appears and links to detail activity

--- app/src/main/java/com/example/android/myapplication/MovieGrid.java
+++ app/src/main/java/com/example/android/myapplication/MovieGrid.java
@@ -1,33 +1,42 @@
 package com.example.android.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MovieGrid extends AppCompatActivity {
+
+    Toast mToast = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_grid);
 
-        Button test_btn = (Button) findViewById(R.id.test_button);
-        test_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDetailsForMovie(1);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                if (mToast!=null) {
+                    mToast.cancel();
+                }
+
+                mToast = Toast.makeText(MovieGrid.this, "" + position,Toast.LENGTH_SHORT);
+                mToast.show();
+
+                Intent intent = new Intent(MovieGrid.this, MovieDetail.class);
+                intent.putExtra(EXTRA_MESSAGE, String.valueOf(position));
+                startActivity(intent);
             }
         });
     }
 
-    void showDetailsForMovie(int movieId) {
-        Context context = this;
-
-        Intent intent = new Intent(context, MovieDetail.class);
-        intent.putExtra("ID", movieId);
-        startActivity(intent);
-    }
 }
