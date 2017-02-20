63d139d60499588654e744719cc8d61a8abae749
karl-denby <karl.denby@gmail.com>
2017-02-20 19:57:52 +0000

    begin to optimize for tablets aka fragments, added files

---
+++ app/src/main/java/com/example/android/myapplication/MovieDetailFragment.java
@@ -0,0 +1,26 @@
+package com.example.android.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieDetailFragment extends Fragment {
+
+    boolean mTabletLayout = getResources().getBoolean(R.bool.isTablet);
+    int mGridSelectedIndex = 0;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        return view;
+    }
+}
